99d7a58d527b50ae21c128d193ab3b988aec778e
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-09-11 11:57:45 +0300

    added review section

--- js/script.js
+++ js/script.js
@@ -24,6 +24,30 @@ var swiper = new Swiper(".home-slider", {
   loop:true,
 });
 
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
 document.addEventListener('DOMContentLoaded', function(){
 
   google.charts.load('current', {'packages':['corechart']});
